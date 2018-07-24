fdecc3e869a3ac0e1b3dd2e9dc386eb2c2ac8237
AMCorvi <am@corvi.com>
2018-07-24 06:44:13 -0400

    initial setup of api route

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express')
+var path = require('path')
+var favicon = require('serve-favicon')
+var logger = require('morgan')
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
 
+var stickers = require('./api/stickers')
 
-var app = express();
+var app = express()
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cookieParser())
+
+
+app.use('/api/v1/stickers', stickers)
 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
 
 // error handler
 app.use(function(err, req, res, next) {
-
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.json({
     message: err.message
-    ,error: ( req.app.get('env') === 'development' ? err : {} )
+    ,error:   req.app.get('env') === 'development' ? err : {}
   })
-});
+})
 
-module.exports = app;
+module.exports = app
