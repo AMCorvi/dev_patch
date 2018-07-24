567b9467c87047cfed7dbabdc99c5b9b1b2c956b
AMCorvi <am@corvi.com>
2018-07-24 08:47:53 -0400

    - Configured test db connection for.... welll testing obviously - Modification to 'app.spec.js' to ensure properly interaction with db - Added 'package.json' scripts to simplify workflow - Updated DEV dependencies

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,26 @@
-module.exports = {development: {
-  client:     'pg'
-  ,connection: {
-    host:     'corax.dev'
-    ,port:     5432
-    ,user: 'type'
-    ,password: 'lit'
-    ,database: 'type'
+module.exports = {
+  development: {
+    client:     'pg'
+    ,connection: {
+      host:     'corax.dev'
+      ,port:     5432
+      ,user:     'type'
+      ,password: 'lit'
+      ,database: 'type'
+    }
+    ,migrations: { directory: __dirname + '/db/migrations' }
+    ,seeds:      { directory: __dirname + '/db/seeds' }
   }
-  ,migrations: { directory: __dirname + '/db/migrations' }
-  ,seeds:       { directory: __dirname + '/db/seeds' }
-}}
+  ,test: {
+    client:     'sqlite'
+    ,connection: {filename: './specs/test.db'
+      //   ,port:     5432
+      //   ,user: 'type'
+      //   ,password: 'lit'
+      //   ,database: 'type'
+    }
+    ,useNullAsDefault: true
+    ,migrations:       { directory: __dirname + '/db/migrations' }
+    ,seeds:            { directory: __dirname + '/db/seeds' }
+  }
+}
